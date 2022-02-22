47d3ca07be93699f3fb005ad27438d15cdd53130
darkwingpatil <91596905+darkwingpatil@users.noreply.github.com>
2022-02-22 20:24:18 +0530

    initial done
    intial

--- index.js
+++ index.js
@@ -44,3 +44,61 @@ i4.addEventListener("click",function(){
 })
 
 document.getElementById("navbar").append(subscrib,image,box)
+
+
+
+document.getElementById("btn1").addEventListener("click",function(){
+    nxtbut();
+})
+
+document.getElementById("btn2").addEventListener("click",function(){
+    prvbut();
+})
+
+var count=1;
+function nxtbut()
+{
+    if(count==4)
+    {
+        count=1;
+    }
+    else
+    {
+        count++;
+    }
+    display()
+}
+
+function prvbut()
+{
+    if(count==1)
+    {
+        count=4;
+    }
+    else
+    {
+        count--;
+    }
+    display()
+}
+
+function display()
+{
+    var val=document.querySelectorAll("#allimg > div");
+    console.log(val.length)
+
+    for(var i=0;i<val.length;i++)
+    {
+        if(i+1==count)
+        {
+            val[i].style.display="block";
+            console.log(val[i])
+        }
+        else
+        {
+            val[i].style.display="none";
+        }
+    }
+}
+
+setInterval(nxtbut,4000)
